5d2ddd9e3cb9675f9d4489eeab7245e959214171
Majid Iqbal <majid@outlook.de>
2020-05-12 13:54:21 +0200

    Add obsolete comments and attributes

--- BlazorGrid.Abstractions/IGridRow.cs
+++ BlazorGrid.Abstractions/IGridRow.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace BlazorGrid.Abstractions
 {
-    [Obsolete]
+    [Obsolete("This interface was necessary for a so-called smart refresh feature. This feature has been deprecated, therefore this interface is no longer necessary.")]
     public interface IGridRow
     {
         string RowId { get; }
